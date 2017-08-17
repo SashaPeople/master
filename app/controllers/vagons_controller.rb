80c76f569b303cd37c949a5a59ec21e5f2ae21a4
SashaPeople <SashaPeople@gmail.com>
2017-08-17 12:41:30 +0300

    Update vagons_controller.rb

--- app/controllers/vagons_controller.rb
+++ app/controllers/vagons_controller.rb
@@ -1,37 +1,23 @@
 class VagonsController < ApplicationController 
-<<<<<<< HEAD
   before_action :set_vagon, only: [:show, :edit, :update, :destroy]
   before_action :set_train, only: [:create, :new]
-=======
-  before_action :set_train, only: [ :new, :create ]
-  before_action :set_vagon, only: [ :update, :show, :edit, :destroy ]
->>>>>>> master_yoda
 
   def new 
     @vagon = Vagon.new 
   end
 
   def show 
-<<<<<<< HEAD
   end
 
   def new 
     @vagon = Vagon.new 
-=======
->>>>>>> master_yoda
   end
 
   def edit 
   end
-
-<<<<<<< HEAD
+  
   def create 
     @vagon = @train.vagons.new(vagon_params)
-=======
-  def create
-    @vagon = @train.vagons.new(vagon_params)
-
->>>>>>> master_yoda
     if @vagon.save
       redirect_to @train
     else 
@@ -49,16 +35,11 @@ class VagonsController < ApplicationController
 
   def destroy 
     @vagon.destroy
-<<<<<<< HEAD
-    redirect_to @vagon.train
-=======
     redirect_to trains_path
->>>>>>> master_yoda
   end
 
   protected 
 
-<<<<<<< HEAD
   def set_train
     @train = Train.find(params[:train_id])
   end
@@ -67,9 +48,6 @@ class VagonsController < ApplicationController
     @vagon = Vagon.find(params[:id])
   end
 
-  def vagon_params
-    params.require(:vagon).permit(:vtype, :train_id, :vnumber)
-=======
   def vagon_params
     params.require(:vagon).permit(:number, :vtype)
   end 
@@ -80,7 +58,6 @@ class VagonsController < ApplicationController
 
   def set_vagon 
     @vagon = Vagon.find(params[:id])
->>>>>>> master_yoda
   end
 
 end
